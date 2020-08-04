a6dcf47ef4f61b1580d043a3ee85472a669384a2
Stéphane HULARD <s.hulard@chstudio.fr>
2020-08-04 09:48:10 +0200

    Implement chat server logic.

--- server.php
+++ server.php
@@ -4,4 +4,5 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = new Ratchet\App('127.0.0.1', 8080);
 $app->route('/echo', new Ratchet\Server\EchoServer, ['*']);
+$app->route('/chat', new CHStudio\IPC\WebSocket\ChatServer, ['*']);
 $app->run();
---
+++ src/ChatServer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CHStudio\IPC\WebSocket;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class ChatServer implements MessageComponentInterface
+{
+    private \SplObjectStorage $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn, new \stdClass);
+    }
+
+    public function onMessage(ConnectionInterface $from, $message)
+    {
+        if (0 === strpos($message, '/name')) {
+            $this->clients[$from]->name = trim(substr($message, 5));
+            return;
+        }
+
+        if (isset($this->clients[$from]->name)) {
+            $message = sprintf('%s: %s', $this->clients[$from]->name, $message);
+        }
+
+        foreach ($this->clients as $client) {
+            if ($client !== $from) {
+                $client->send($message);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        $conn->close();
+        throw $e;
+    }
+}
